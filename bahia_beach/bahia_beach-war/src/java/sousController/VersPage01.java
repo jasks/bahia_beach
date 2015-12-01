1688f6f91254ffa5dddfc28bf2317ee55d7027db
cdi418 <cdi418@pltz821.AFPA-PARIS-DI>
2015-12-01 10:07:53 +0100

    MVC2 READY DAMN !

---
+++ bahia_beach/bahia_beach-war/src/java/sousController/VersPage01.java
@@ -0,0 +1,22 @@
+
+package sousController;
+
+import java.io.Serializable;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author cdi418
+ */
+public class VersPage01 implements ControllerInterface, Serializable{
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response, HttpServlet servlet) {
+      String s = "dans versPage01";
+      request.setAttribute("msg", s);
+      return "/WEB-INF/mvc2/page01.jsp";
+    }
+    
+}
---
+++ bahia_beach/bahia_beach-war/web/WEB-INF/mvc2/index.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>MVC2 IS READY</h1>
+        <a href="Controller?section=page01">lien vers page 01</a>
+        <a href="Controller?section=page02">lien vers page 02</a>
+    </body> 
+</html>
