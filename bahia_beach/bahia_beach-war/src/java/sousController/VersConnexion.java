d784c3a89f91d62a7c6b9aebe9e8a2e2bcf105c7
CDI405 <CDI405@pltz889.AFPA-PARIS-DI>
2015-12-03 16:48:56 +0100

    Merge origin/master
    Conflicts:
    	bahia_beach/bahia_beach-ejb/src/java/inputBDD/beanEssaie.java

--- bahia_beach/bahia_beach-war/src/java/sousController/VersConnexion.java
+++ bahia_beach/bahia_beach-war/src/java/sousController/VersConnexion.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class VersConnexion implements ControllerInterface, Serializable{
+public class VersConnexion implements ControllerInterface, Serializable {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response, HttpServlet servlet) {
       String s = "dans versConnexion";
